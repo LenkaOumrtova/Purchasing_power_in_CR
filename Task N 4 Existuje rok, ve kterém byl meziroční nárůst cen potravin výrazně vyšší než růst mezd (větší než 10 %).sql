a3b82b80294271bacc0c7947b82cb5f838000d86
LenkaOumrtova <126987366+LenkaOumrtova@users.noreply.github.com>
2023-04-30 13:13:25 +0200

    Update Task N 4 Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %).sql

--- Task N 4 Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %).sql
+++ Task N 4 Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %).sql
@@ -1,30 +1,15 @@
-SELECT 
-	t.payroll_year,
-	CASE 
-		WHEN min_food_price_growth - min_salary_growth >= 10 THEN hight
-		ELSE low
-	END AS  differenc_growth_prices_salary
-FROM
-(SELECT 
-	t.food_name,
-	t.payroll_year,
-	t.payroll_year + 1 AS t.next_year,
-	t.food_price,
-	round (min((t2.food_price - t.food_price) / t.food_price * 100, 1)) AS food_price_growth
-FROM t_lenka_oumrtova_project_sql_primary_final t
-JOIN t_lenka_oumrtova_project_sql_primary_final t2
-	ON t.food_code = t2.food_code   
-GROUP BY t.food_name) AS prices
-JOIN
-(SELECT
-	ts.average_salary,
-	ts.payroll_year,
-	ts.payroll_year + 1 AS ts.next_year,
-	round (min((ts2.average_salary - ts.average_salary) / ts.average_salary * 100, 1)) AS min_salary_growth
-FROM t_lenka_oumrtova_project_sql_primary_final ts
-JOIN t_lenka_oumrtova_project_sql_primary_final ts2
-	ON ts.statistical_variable = ts2.statistical_variable 
-GROUP BY ts.average_salary) AS salary
-ON prices.payroll_year = salary.payroll_year
-ORDER BY t.next_year
+/*
+ * č. 4 Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+ */
+
+
+SELECT
+	t.`year`,
+	t1.`year` AS previous_year,
+	round((t.food_price - t1.food_price) / t1.food_price * 100, 1) AS food_price_growth,
+	round((t.average_salary - t1.average_salary) / t1.average_salary * 100, 1) AS salary_growth
+FROM t_lena_oumrtova_project_sql_primary_final t
+JOIN t_lena_oumrtova_project_sql_primary_final t1 
+	ON t.`year` = t1.`year` + 1
+GROUP BY `year`
 ;
