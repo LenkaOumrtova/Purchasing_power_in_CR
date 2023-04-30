f643dfb5306342ec3e40ab802afa19e63fcd42dd
LenkaOumrtova <126987366+LenkaOumrtova@users.noreply.github.com>
2023-04-30 13:05:59 +0200

    Update Task N 2 Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd.sql

--- Task N 2 Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd.sql
+++ Task N 2 Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd.sql
@@ -1,24 +1,39 @@
+/* č. 2 Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období 
+ * v dostupných datech cen a mezd?
+ * 
+ * první srovnávané období je rok 2006
+ * poslední srovnávané období je rok 2018
+ */
+
+/*
+ * chléb
+ */
 SELECT 
 	average_salary,
-	payroll_year,
+	`year`,
 	food_name,
 	food_price,
-	round(average_salary / food_price) AS bread_number
-FROM t_lenka_oumrtova_project_sql_primary_final tlopspf 
+	round((average_salary / food_price),0) AS bread_number
+FROM t_lena_oumrtova_project_sql_primary_final
 WHERE food_name LIKE ('%chléb%')
-	AND payroll_year IN (2006, 2018)
-GROUP BY payroll_year
+	AND `year` IN (2006, 2018)
+GROUP BY `year`
 ;
 
+
+/*
+ * mléko
+ */
 
 SELECT 
 	average_salary,
-	payroll_year,
+	`year`,
 	food_name,
 	food_price,
-	round(average_salary / food_price) AS milk_number
-FROM t_lenka_oumrtova_project_sql_primary_final tlopspf 
+	round((average_salary / food_price),0) AS milk_number
+FROM t_lena_oumrtova_project_sql_primary_final
 WHERE food_name LIKE ('%mléko%')
-	AND payroll_year IN (2006, 2018)
-GROUP BY payroll_year
+	AND `year` IN (2006, 2018)
+GROUP BY `year`
 ;
+
