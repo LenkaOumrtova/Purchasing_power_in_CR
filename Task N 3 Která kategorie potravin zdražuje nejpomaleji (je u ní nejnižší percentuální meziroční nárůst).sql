d5e1b03bdc779a532b416a5a8f6b3d58d02da874
LenkaOumrtova <126987366+LenkaOumrtova@users.noreply.github.com>
2023-04-30 13:12:57 +0200

    Update Task N 3 Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst).sql

--- Task N 3 Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst).sql
+++ Task N 3 Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst).sql
@@ -1,13 +1,14 @@
+/*
+ * č. 3 Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
+ */
 
-SELECT 
+SELECT
 	t.food_name,
-	t.payroll_year,
-	t.payroll_year + 1 AS next_year,
-	round (min((t2.food_price - t.food_price) / t.food_price * 100, 1)) AS food_price_growth
-FROM t_lenka_oumrtova_project_sql_primary_final t
-JOIN t_lenka_oumrtova_project_sql_primary_final t2
-	ON t.food_code = t2.food_code 
-	AND t.payroll_year = t2.payroll_year + 1
-	AND t.payroll_year <=2018 
-ORDER BY food_name
+	t.`year`,
+	t1.`year` AS previous_year,
+	round((t.food_price - t1.food_price) / t1.food_price * 100, 1) AS food_price_growth
+FROM t_lena_oumrtova_project_sql_primary_final t
+JOIN t_lena_oumrtova_project_sql_primary_final t1 
+	ON t.`year` = t1.`year` + 1
+ORDER BY food_price_growth
 ;
